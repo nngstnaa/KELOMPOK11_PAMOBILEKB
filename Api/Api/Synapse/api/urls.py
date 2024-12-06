6f2b722132b0a0f8c775907186749a3bc7222667
nngstnaa <145427261+nngstnaa@users.noreply.github.com>
2024-12-07 00:28:44 +0800

    Delete Api/Api/Synapse directory

--- Api/Api/Synapse/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import predict, predict_image
-
-urlpatterns = [
-    path('predict', predict, name='predict'),
-    path('predict-image', predict_image, name='predict_image')
-]
